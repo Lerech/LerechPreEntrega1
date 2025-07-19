313377f444aecc631dad4238a2b70078d54895be
Lerech <nicolas.lerech@gmail.com>
2025-07-19 03:05:13 -0300

    primer commit

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ alert("Buenas, bienvenido a mi primer pre entrega!!!")
 let nombre = prompt("¿Cual es tu nombre?")
 
 console.log (nombre)
+
+let edad = prompt("¿Cuantos años tienes?")
+console.log(edad)
+
+while (edad < 18) {
+    alert("Lo siento, debes ser mayor de edad para continuar.")
+    break;
+} if (edad >= 18) {
+    alert("¡Bienvenido, " + nombre + "! Puedes continuar.")
+
+    }
